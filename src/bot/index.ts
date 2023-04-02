d35421fca9e844c9776e18ad54984a2ad1a47ecf
Yokotes <takjchannel@gmail.com>
2023-04-02 20:01:23 +0300

    fix: add try catch to prevent bot crushing

--- src/bot/index.ts
+++ src/bot/index.ts
@@ -49,9 +49,11 @@ export class TelegramBot {
     });
 
     // TODO: Throws strange error on unformatted message
-    this.bot.on("message", async ({ chat, text }) => {
+    this.bot.on("message", async ({ chat, text, date }) => {
       if (!text) return;
-      console.log(text);
+      // To prevent extra replies (I really don't know why `date` doesn't have last 3 numbers...)
+      const now = new Date().getTime().toString();
+      if (+now.slice(0, now.length - 3) > date) return;
 
       const name = this.data[chat.id];
       const splitedText = text.split(",");
@@ -61,7 +63,7 @@ export class TelegramBot {
       if (name?.toLowerCase() !== possibleName.toLowerCase()) return;
 
       const response = await this.aiService.sendRequest(requestText);
-      console.log(response);
+
       this.bot.sendMessage(chat.id, response);
     });
   }
